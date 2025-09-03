ad0fef16316a9d4b5b4ab2bc6477fba5f664370e
alumno <alumno@Instituto.local>
2025-09-03 08:04:02 -0300

    03/08/25

--- api.js
+++ api.js
@@ -1,32 +1,52 @@
-let titulo = document.querySelector('h1')
-let img = document.querySelector('img')
-let siguiente = document.querySelector('#siguiente')
-let anterior = document.querySelector('#anterior')
-let num = 20
+let titulo = document.querySelector('#titulo');
+let imagen = document.querySelector('#imagen');
+let botonMostrar = document.querySelector('#mostrar');
+let botonAtras = document.querySelector('#atras');
+let botonAdelante = document.querySelector('#adelante');
 
-function mostrar(numero){
-    fetch('https://pokeapi.co/api/v2/pokemon-form/' + numero)
+let idActual = 1;
+const MAX_POKEMON = 898;
+const MIN_POKEMON = 1;
 
-        .then(respuesta => respuesta.json())
-
-        .then(datos => {
-            titulo.textContent = datos.name
-            img.src = datos.sprites.front_default
+function mostrarPokemon(id) {
+    fetch(`https://pokeapi.co/api/v2/pokemon-form/${id}/`)
+        .then(respuesta => {
+            if (!respuesta.ok) throw new Error("No se pudo obtener el Pokémon");
+            return respuesta.json();
         })
+        .then(datos => {
+            titulo.textContent = datos.name.charAt(0).toUpperCase() + datos.name.slice(1);
+            imagen.src = datos.sprites.front_shiny;
+            imagen.alt = `Imagen de ${datos.name}`;
+        })
+        .catch(error => {
+            titulo.textContent = 'Error al cargar Pokémon';
+            imagen.src = '';
+            imagen.alt = 'Error';
+            console.error(error);
+        });
 }
 
+// Mostrar uno aleatorio
+botonMostrar.onclick = function () {
+    idActual = Math.floor(Math.random() * MAX_POKEMON) + MIN_POKEMON;
+    mostrarPokemon(idActual);
+};
 
-siguiente.onclick = function () {
-    //aca va todo lo que hace el boton
-    num++
-    mostrar(num)
-}
+// Pokémon anterior
+botonAtras.onclick = function () {
+    idActual--;
+    if (idActual < MIN_POKEMON) idActual = MAX_POKEMON;
+    mostrarPokemon(idActual);
+};
 
+// Pokémon siguiente
+botonAdelante.onclick = function () {
+    idActual++;
+    if (idActual > MAX_POKEMON) idActual = MIN_POKEMON;
+    mostrarPokemon(idActual);
+};
 
-anterior.onclick = function () {
-    //aca va todo lo que hace el boton
-    num--
-    mostrar(num)
-}
-
+// Mostrar el primero al cargar
+mostrarPokemon(idActual);
 
